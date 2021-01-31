f8ac1a5a33e1dd760a77483cdeb4629964457358
mellbo <office@upx.ro>
2021-01-31 15:29:16 +0200

    Add files via upload

--- app07florin/js/script.js
+++ app07florin/js/script.js
@@ -69,23 +69,28 @@ function force_update(){
 /*REGULAR*/
 /*------------------------------------------------------------------------------------------------*/
 function createZonehtml() {
-	/*
-	%ZONEx_TEMP%
-	%ZONEx_SETTEMP%
-	%ZONEx_HEATING%
-	%ZONEx_NAME%
-	*/
+	let dbL = document.getElementById("idLiveDataStorage")[0].value;
+	let dbS = document.getElementById("idTempSetInit")[0].value;
+	let dbH = document.getElementById("idZoneHeatingInit")[0].value;
+	let dbN = document.getElementById("idZoneName")[0].value;
+	
 	let elRowContainer = document.getElementById("idRowContainer");
 	let html_code = [];
 	
-	for (let i = 0; i < 15; i++) {
+	for (let i = 1; i <= 15; i++) {
+		let xHeat = dbH.split("::")[i-1];
+		let clsHeat = xHeat ? "d-none" : "";
+		let xRoomTemp = dbL.split("::")[i-1];
+		let xSetTemp = dbS.split("::")[i-1];
+		let xName = dbN.split("::")[i-1];
+		
 		html_code.push('<div class="col-sm-6 col-md-4 mb-4">');
 		html_code.push('<div class="card lcd_style">');
 		html_code.push('<div class="card-body mb-0 p-0">');
-		html_code.push('<i class="fa fa-fire lcd_icon mr-2 d-none" id="idHeatOn-Zone'+i.toString()+'"></i>');
-		html_code.push('<h4 class="card-title lcd_font1 m-0 text-center" id="idRoomTemp-Zone'+i.toString()+'">%ZONE'+i.toString()+'_TEMP%</h4>');
-		html_code.push('<h3 class="mb-0 lcd_font2">%ZONE'+i+'_NAME%</h3>');
-		html_code.push('<h3 id="idCurSetTemp-Zone'+i+'" class="mb-0 lcd_font3">%ZONE'+i.toString()+'_SETTEMP%</h3>');
+		html_code.push('<i class="fa fa-fire lcd_icon mr-2 '+clsHeat+'" id="idHeatOn-Zone'+i.toString()+'"></i>');
+		html_code.push('<h4 class="card-title lcd_font1 m-0 text-center" id="idRoomTemp-Zone'+i.toString()+'">'+xRoomTemp+'</h4>');
+		html_code.push('<h3 class="mb-0 lcd_font2">'+xName+'</h3>');
+		html_code.push('<h3 id="idCurSetTemp-Zone'+i+'" class="mb-0 lcd_font3">'+xSetTemp+'</h3>');
 		html_code.push('<i class="fa fa-backward icon_arrow_left" id="idTempDown-Zone'+i.toString()+'"></i>');
 		html_code.push('<i class="fa fa-forward icon_arrow_right" id="idTempUp-Zone'+i.toString()+'"></i>');
 		html_code.push('</div></div></div>');
@@ -93,7 +98,8 @@ function createZonehtml() {
 	elRowContainer.innerHTML = html_code.join("");
 	html_code = null;
 	
-	for (let i = 0; i < 15; i++) {		
+	/*addEventListener*/
+	for (let i = 1; i <= 15; i++) {		
 		let idDownZone = 'idTempDown-Zone'+i.toString();
 		let idCurentSetTemp = 'idCurSetTemp-Zone'+i.toString();		
 		document.getElementById(idDownZone).addEventListener('click', function(e){
