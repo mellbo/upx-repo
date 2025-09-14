dae84c2b14c3361fd7d0e640d4eb03dc4acbd3b2
GILBERT <office@upx.ro>
2025-09-14 15:33:28 +0300

    Update myJavascript.js

--- SMARTHOME-REPO/web-repo/assets/js/myJavascript.js
+++ SMARTHOME-REPO/web-repo/assets/js/myJavascript.js
@@ -216,6 +216,18 @@ function checkMillis() {
 function inject_function_settings() {
   console.log("loading action and function settings");   
   /*SET ACTION*/
+  
+    /* QUICK SAVING SEND */
+    $("#set_THERMOSTAT_OUTSIDE_ENABLE").on("change", function(e) {
+      console.log("set_THERMOSTAT_OUTSIDE_ENABLE:", $('#set_THERMOSTAT_OUTSIDE_ENABLE').is(":checked"));
+      let data = {
+          "THERMOSTAT_OUTSIDE_ENABLE": $('#set_THERMOSTAT_OUTSIDE_ENABLE').is(":checked")
+        };
+      let _js = JSON.stringify(data);	
+      if (websck_is_connected) websocket.send(_js);
+      _js	= null; data = null;          
+    });
+    //-->
     //set_NewTEMPInCALL
     $('#set_NewTEMPInCALL').slider({
         id: 'ex1Slider', //class
