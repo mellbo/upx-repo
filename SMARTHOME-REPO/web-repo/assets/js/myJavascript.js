e1359dc03121dc7d571055b0ec45e8a1730d8a77
GILBERT <office@upx.ro>
2025-09-16 02:24:32 +0300

    Update myJavascript.js

--- SMARTHOME-REPO/web-repo/assets/js/myJavascript.js
+++ SMARTHOME-REPO/web-repo/assets/js/myJavascript.js
@@ -253,7 +253,7 @@ function inject_function_settings() {
       let _js = JSON.stringify(data);	
       if (websck_is_connected) websocket.send(_js);
       _js	= null; data = null;
-      $('#smartWelcomeWindow').addClass('in');
+      $('#smartWelcomeWindow').removeClass('in');
     });
     
     // PREFERED_LIGHT_DORMITOR
